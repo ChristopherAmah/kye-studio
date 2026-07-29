2624f2daa6acf016fc196f119c5fc1c9a09c48b1
Christopher Amah <christopheramah0@gmail.com>
2026-07-29 11:27:09 +0100

    vercel and redirect corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import './App.css'
 import Overview from './pages/Overview'
 import Navbar from './components/Navbar'
@@ -7,19 +7,22 @@ import Home from './pages/Home'
 import Footer from './pages/Footer'
 import Works from './pages/Works'
 
-
 function App() {
-
   return (
     <>
-    <Navbar />
-    <Routes>
-      <Route path='/' element={<Overview />} />
-      <Route path='/home' element={<Home />} />
-      <Route path='/works' element={<Works />} />
-      
-    </Routes>
-    <Footer />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Overview />} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/works' element={<Works />} />
+
+        {/* Option A: Render Overview directly on invalid route */}
+        <Route path="*" element={<Overview />} />
+
+        {/* Option B: If you prefer to redirect the actual URL back to '/' */}
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
+      </Routes>
+      <Footer />
     </>
   )
 }
